56cdb8487b61aa2204b1bd79c2ebf7cfe7bd6828
Zion-Webiks <zion@webiks.com>
2024-11-25 17:20:10 +0200

    App config & Routes for Users Operations

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from "express"
+import cros from "cors"
+import "dotenv/config"
+import userRouter from "./routers/userRouter";
+import { connectToMongo } from "./config/db";
+import verifyUser from "./middleware/verifyUser";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express()
+connectToMongo()
+
+app.use(express.json())
+app.use(cros())
+
+app.use("/api/users",verifyUser, userRouter);
+app.use("/api/auth", userRouter);
+
+app.listen(PORT, () =>  {
+  console.log(`Server started, Visit "http://localhost:${PORT}"`);
+})
